aebed05d164c9dfb5a2469fcdabb0e34155fcfd2
Kampty <94106000+kampty@users.noreply.github.com>
2025-07-28 13:47:30 +0300

    Update index.tsx

--- src/components/CardsContent/me/index.tsx
+++ src/components/CardsContent/me/index.tsx
@@ -29,12 +29,12 @@ function AboutCard({ isMobile }: {
       <div className={styles.text}>
         <h2 dangerouslySetInnerHTML={{ __html: translation.HEADING }} />
         <div className={styles.imgBlock}>
-          <img src=-sticker1 alt={translation.INTRO_IMAGE} align='right' width={isMobile ? 80 : 130} />
+          <img src={sticker1} alt={translation.INTRO_IMAGE} align='right' width={isMobile ? 80 : 130} />
           <p dangerouslySetInnerHTML={{ __html: dates(translation.INTRO) }} />
         </div>
         <p dangerouslySetInnerHTML={{ __html: dates(translation.PARAGRAPH_2) }} />
         <div className={styles.imgBlock}>
-          <video src=-sticker2 autoPlay width={isMobile ? 50 : 170} muted loop alt={translation.MY_PROJECTS_IMAGE} />
+          <video src={sticker2} autoPlay width={isMobile ? 50 : 170} muted loop alt={translation.MY_PROJECTS_IMAGE} />
           <p dangerouslySetInnerHTML={{ __html: translation.MY_PROJECTS }} />
         </div>
         <p dangerouslySetInnerHTML={{ __html: translation.FEATURED }} />
